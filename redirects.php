bc8390047fbc75a2273967aa896d917ca09bfb9d
ousamaya <35897222+ousamaya@users.noreply.github.com>
2018-03-06 11:46:03 +0100

    Create redirects.php

---
+++ redirects.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+* Name of developer: Ousama Yamine
+* Website: www.yawaweb.com
+* Donate BTC: 1BoBFr6pYh3phS8iEs5jxxN2cihYxuMdgW
+* Donate ETH: 0xf197d28c9053C4fdfef6794CD80bcbf8886caAD9
+* Donate NEO: 
+**/
+
+$myhomepage = 'https://your-homepage.com'; //Your Startpage
+
+$affid = htmlspecialchars($_GET["affid"]); //Do not changing
+
+$affLinkList = array( //Insert here the links do you want mask
+"affiliatelink1" => "https://www.example.com",
+"affiliatelink2" => "https://www.test.com"
+);
+
+$link = array_key_exists($affid, $affLinkList) === false ? $myhomepage : $affLinkList[$affid]; //Checks if the Link exists if not then redirect to your Homepage
+
+
+header("Location: $link"); //Do not changing
+exit(); //Do not changing
